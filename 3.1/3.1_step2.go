df0375724e85f01482df29d4581b2fc5ea71459b
ipetrash <ilya.petrash@inbox.ru>
2020-10-14 04:14:23 +0500

    Refactoring

--- 3.1/3.1_step2.go
+++ 3.1/3.1_step2.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -46,4 +47,17 @@ func main() {
 	}
 
 	fmt.Println()
+
+	cityPopulation := map[string]int{
+		"A_10": 10, "B_10": 15, "C_10": 20,
+		"A_100": 100, "B_100": 200, "C_100": 400,
+		"A_1000": 1001, "B_1000": 2000, "C_1000": 3000,
+	}
+	for k := range cityPopulation {
+		if strings.HasPrefix(k, "C_") {
+			delete(cityPopulation, k)
+		}
+	}
+	fmt.Println(cityPopulation)
+	// map[A_10:10 A_100:100 A_1000:1001 B_10:15 B_100:200 B_1000:2000]
 }

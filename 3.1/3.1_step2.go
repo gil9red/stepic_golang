57c788bd310c21406416ae27f3dde4cde73e5d3e
ipetrash <ilya.petrash@inbox.ru>
2020-10-14 01:23:31 +0500

    Added 3.1/3.1_step2.go

---
+++ 3.1/3.1_step2.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	m := map[int]int{
+		12: 2,
+		1:  5,
+	}
+	fmt.Println(m)      // map[1:5 12:2]
+	fmt.Println(len(m)) // 2
+	fmt.Println(m[12])  // 2
+
+	fmt.Println()
+
+	for key, value := range m {
+		fmt.Printf("for. key: %v, value: %v\n", key, value)
+	}
+
+	fmt.Println()
+
+	var value int
+	var hasValue bool
+
+	value, hasValue = m[404]
+	fmt.Println(value, hasValue) // 0 false
+
+	value, hasValue = m[1]
+	fmt.Println(value, hasValue) // 5 true
+
+	fmt.Println()
+
+	delete(m, 12)
+	fmt.Println(m) // map[1:5 12:2]
+
+	fmt.Println()
+
+	if value, inMap := m[1]; inMap {
+		fmt.Println("key: 1, value:", value) // 10
+	}
+
+	if value, inMap := m[404]; inMap {
+		fmt.Println("key: 404, value:", value) // Условие не выполняется
+	}
+
+	fmt.Println()
+}

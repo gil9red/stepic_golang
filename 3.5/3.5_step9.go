afba5f3271da4774d49f03f3ff1b6f0de5ec3573
ipetrash <ilya.petrash@inbox.ru>
2020-10-25 07:22:39 +0500

    Refactoring

--- 3.5/3.5_step9.go
+++ 3.5/3.5_step9.go
@@ -36,6 +36,9 @@ func main() {
 
 	num := 0
 	for in.Scan() {
+		if len(in.Text()) == 0 {
+			break
+		}
 		n, err := strconv.Atoi(in.Text())
 		if err != nil {
 			panic(err)
@@ -44,7 +47,12 @@ func main() {
 		num += n
 	}
 
-	out := bufio.NewWriter(os.Stdout)
-	_, _ = out.WriteString(strconv.Itoa(num))
-	_ = out.Flush()
+	_, _ = os.Stdout.WriteString(strconv.Itoa(num))
+	// OR:
+	//_, _ = io.WriteString(os.Stdout, strconv.Itoa(num))
+	//
+	// OR:
+	//out := bufio.NewWriter(os.Stdout)
+	//_, _ = out.WriteString(strconv.Itoa(num))
+	//_ = out.Flush()
 }

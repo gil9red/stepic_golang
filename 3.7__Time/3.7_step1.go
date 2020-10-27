9870cf98e19e105ea53dd75c29fe4acced6db10e
ipetrash <ilya.petrash@inbox.ru>
2020-10-27 12:40:21 +0500

    Refactoring

--- 3.7__Time/3.7_step1.go
+++ 3.7__Time/3.7_step1.go
@@ -6,6 +6,33 @@ import (
 )
 
 func main() {
+	// stdLongMonth      = "January"
+	// stdMonth          = "Jan"
+	// stdNumMonth       = "1"
+	// stdZeroMonth      = "01"
+	// stdLongWeekDay    = "Monday"
+	// stdWeekDay        = "Mon"
+	// stdDay            = "2"
+	// stdUnderDay       = "_2"
+	// stdZeroDay        = "02"
+	// stdHour           = "15"
+	// stdHour12         = "3"
+	// stdZeroHour12     = "03"
+	// stdMinute         = "4"
+	// stdZeroMinute     = "04"
+	// stdSecond         = "5"
+	// stdZeroSecond     = "05"
+	// stdLongYear       = "2006"
+	// stdYear           = "06"
+	// stdPM             = "PM"
+	// stdpm             = "pm"
+	// stdTZ             = "MST"
+	// stdISO8601TZ      = "Z0700"  // prints Z for UTC
+	// stdISO8601ColonTZ = "Z07:00" // prints Z for UTC
+	// stdNumTZ          = "-0700"  // always numeric
+	// stdNumShortTZ     = "-07"    // always numeric
+	// stdNumColonTZ     = "-07:00" // always numeric
+
 	// time.Now | time.Date | time.Unix | Format
 	{
 		// func Now() Time

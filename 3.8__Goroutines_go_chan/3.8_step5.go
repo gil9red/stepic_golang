c78d475f15a4c48f5d0d0c06382175e5c9f28f9b
ipetrash <ilya.petrash@inbox.ru>
2020-10-30 11:54:10 +0500

    Added 3.8__Goroutines_go_chan/3.8_step5.go

---
+++ 3.8__Goroutines_go_chan/3.8_step5.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	const N = 10
+
+	fmt.Println("Started!")
+
+	worker := func(wg *sync.WaitGroup, c chan string, name string, timeout time.Duration) {
+		defer wg.Done()
+		wg.Add(1)
+
+		for i := 1; i <= N; i++ {
+			c <- fmt.Sprintf("[%v] %v", name, i)
+			time.Sleep(timeout)
+		}
+	}
+
+	monitorWorker := func(wg *sync.WaitGroup, cs chan string) {
+		wg.Wait()
+		close(cs)
+	}
+
+	wg := &sync.WaitGroup{}
+	c := make(chan string, 10)
+
+	go worker(wg, c, "a", time.Millisecond*100)
+	go worker(wg, c, "b", time.Millisecond*300)
+	go worker(wg, c, "c", time.Millisecond*50)
+
+	go monitorWorker(wg, c)
+
+	for x := range c {
+		fmt.Println(x)
+	}
+
+	fmt.Println("Finish!")
+}

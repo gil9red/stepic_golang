a283799b4066094ef9be75e4ea2156d8e7146ca2
ipetrash <ilya.petrash@inbox.ru>
2020-10-30 14:24:50 +0500

    Refactoring

--- 3.8__Goroutines_go_chan/3.8_step5.go
+++ 3.8__Goroutines_go_chan/3.8_step5.go
@@ -32,7 +32,6 @@ func main() {
 
 		worker := func(wg *sync.WaitGroup, c chan string, name string, timeout time.Duration) {
 			defer wg.Done()
-			wg.Add(1)
 
 			for i := 1; i <= N; i++ {
 				c <- fmt.Sprintf("[%v] %v", name, i)
@@ -48,8 +47,13 @@ func main() {
 		wg := &sync.WaitGroup{}
 		c := make(chan string, 10)
 
+		wg.Add(1)
 		go worker(wg, c, "a", time.Millisecond*100)
+
+		wg.Add(1)
 		go worker(wg, c, "b", time.Millisecond*300)
+
+		wg.Add(1)
 		go worker(wg, c, "c", time.Millisecond*50)
 
 		go monitorWorker(wg, c)

f57c0aef4bd5055993a3e185ecf20b9ed06b0377
ipetrash <ilya.petrash@inbox.ru>
2020-10-30 11:54:10 +0500

    Added 3.8__Goroutines_go_chan/3.8_step5.go

--- 3.8__Goroutines_go_chan/3.8_step5.go
+++ 3.8__Goroutines_go_chan/3.8_step5.go
@@ -7,36 +7,56 @@ import (
 )
 
 func main() {
-	const N = 10
-
 	fmt.Println("Started!")
 
-	worker := func(wg *sync.WaitGroup, c chan string, name string, timeout time.Duration) {
-		defer wg.Done()
-		wg.Add(1)
+	{
+		c := make(chan string)
 
-		for i := 1; i <= N; i++ {
-			c <- fmt.Sprintf("[%v] %v", name, i)
-			time.Sleep(timeout)
+		f := func(c chan string) {
+			c <- "1"
+			c <- "2"
+			c <- "3"
+			close(c)
 		}
+		go f(c)
+
+		for i := 0; i < 3; i++ {
+			fmt.Println(<-c)
+		}
+
+		fmt.Println()
 	}
 
-	monitorWorker := func(wg *sync.WaitGroup, cs chan string) {
-		wg.Wait()
-		close(cs)
-	}
+	{
+		const N = 5
 
-	wg := &sync.WaitGroup{}
-	c := make(chan string, 10)
+		worker := func(wg *sync.WaitGroup, c chan string, name string, timeout time.Duration) {
+			defer wg.Done()
+			wg.Add(1)
 
-	go worker(wg, c, "a", time.Millisecond*100)
-	go worker(wg, c, "b", time.Millisecond*300)
-	go worker(wg, c, "c", time.Millisecond*50)
+			for i := 1; i <= N; i++ {
+				c <- fmt.Sprintf("[%v] %v", name, i)
+				time.Sleep(timeout)
+			}
+		}
 
-	go monitorWorker(wg, c)
+		monitorWorker := func(wg *sync.WaitGroup, cs chan string) {
+			wg.Wait()
+			close(cs)
+		}
 
-	for x := range c {
-		fmt.Println(x)
+		wg := &sync.WaitGroup{}
+		c := make(chan string, 10)
+
+		go worker(wg, c, "a", time.Millisecond*100)
+		go worker(wg, c, "b", time.Millisecond*300)
+		go worker(wg, c, "c", time.Millisecond*50)
+
+		go monitorWorker(wg, c)
+
+		for x := range c {
+			fmt.Println(x)
+		}
 	}
 
 	fmt.Println("Finish!")

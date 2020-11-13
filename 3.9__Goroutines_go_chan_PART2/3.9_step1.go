300fab1a6f9d771c6dee4b8409b6ec3a255ef1d4
ipetrash <ilya.petrash@inbox.ru>
2020-11-13 23:09:03 +0500

    Added 3.9__Goroutines_go_chan_PART2/3.9_step1.go

---
+++ 3.9__Goroutines_go_chan_PART2/3.9_step1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Started!")
+
+	// Select-case
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fibonacci(c, quit)
+
+	fmt.Println("Finish!")
+}
